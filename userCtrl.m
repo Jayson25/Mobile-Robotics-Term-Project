d6cc11b71d6c94ad79d3504c306a27a0d72f6f9e
Jayson255 <jayson.galante@utt.fr>
2017-12-05 03:09:03 +0900

    Add files via upload
    PID controller working

--- userCtrl.m
+++ userCtrl.m
@@ -3,66 +3,68 @@
 % Your control function
 
 function [u, userStructure] = userCtrl(model, environment, userStructure)
-
-    % previously defined value as an example at userInit function
-    % used as a global variable, so that this will change as 0, 1, 2, ....
-    % at every iteration
-    userStructure.exampleVariable = userStructure.exampleVariable + 1;
     
-%     1st approach: accelerate (from rest) until midpoint, then decelerate
-%     with same magnitude. 
-%     Pros: robot almost stops at the intended location.
-%     Cons: has a small drift velocity at the end. robot does not
-%     completely stop. 
-%
-%     if model.state(1) > (endx+startx)/2
-%         u = [-0.1; 0.1];
-%     elseif model.state(1) == endx
-%         u = [0; 0];
-%     else
-%         u = [0.1; 0.1];
-%     end
-            
-%     2nd approach: accelerate car (doesn't matter the acceleration), then
-%     when close to goal, decelerate really quickly. Set acc = -velocity.
-%     Might need to cap the velocity to something lower than 2m/s.
-%     Pros: robot actually comes to a full stop 
-%     Cons: robot is kind of far from goal.
-%
-%     if modelState ~= endx-1
-%         u = [0.1; 0.1];
-%     end
-%     if modelState >= endx-1 && model.state(4)~=0 && model.state(5)~=0
-%         u = [model.state(4)*-1; model.state(5)*-1];
-%     end
-%     if modelState >= endx-1 && model.state(4)==0 && model.state(5)==0
-%         u = [0; 0];
-%     end
-
-%     Maybe we can combine both approaches
-
-    % module that tests translation of robot 
-    startx = userStructure.startPoint(1);
-    endx = 2;
-            
-    if model.state(1) > ((endx + startx)/2.0) && model.state(1) < endx 
-        u = [-0.1; -0.1];
-    elseif model.state(1) >= endx
-        u = [model.state(4)*-50; model.state(5)*-50]; 
+    %checkpoint position x and y 
+    objx = userStructure.checkpoint(userStructure.ci, 1);
+    objy = userStructure.checkpoint(userStructure.ci, 2);
+    
+    %distance betweent the x and y
+    dx = objx-model.state(1);
+    dy = objy-model.state(2);
+    
+    dist = sqrt(dx^2 + dy^2);
+    theta = atan2(dy,dx);
+    
+    timeStep = 0.01;
+    
+    userStructure.errorAngle(size(userStructure.errorAngle)+1) = theta - model.state(3);
+    userStructure.errorDist(size(userStructure.errorDist)+1) = dist;
+    
+    eA = userStructure.errorAngle;
+    eD = userStructure.errorDist;
+    sErrA = size(userStructure.errorAngle);
+    sErrD = size(userStructure.errorDist);
+    
+    userStructure.integDist = userStructure.integDist + eD(sErrD(2))*0.01;
+    userStructure.integAngle = userStructure.integAngle + eA(sErrA(2))*0.01;
+    
+    Kp = 0.45;
+    Ki = 0;
+    Kd = 1;
+    
+    output = pid(eD,Kp,Ki,Kd,timeStep,userStructure.integDist);
+    
+    if eA(sErrA(2)) > 0 && userStructure.started == 0
+        output = 0;
     else
-        u = [0.1; 0.1]
+        userStructure.started = 1;
     end
     
+    u = [output;output];
     
-    % module that tests turning left of robot
-%     startAngle = userStructure.startAngle;
-%     endAngle = 3.14159;
-%     if model.state(3)> (endAngle + startAngle)/2 && model.state(3) < endAngle
-%         u = [0.01; -0.01];
-%     elseif model.state(3) >= endAngle
-%         u = [model.state(4)*-50; model.state(5)*-50];
-%     else
-%         u = [-0.01; 0.01];
-%     end
-%     u   
+    Kp = 1.75;
+    
+    output = pid(eA,Kp,Ki,Kd,timeStep,userStructure.integAngle);
+    
+    u = [u(1)-output;u(2)+output];
+    
+    sci = size(userStructure.checkpoint);
+    
+    if eD(sErrD(2)) < 0.55 && userStructure.ci < sci(1)
+       
+       userStructure.integDist = 0; 
+       userStructure.integAngle = 0;
+       
+       userStructure.errorDist = 0;
+       userStructure.errorAngle = 0;
+       
+       if userStructure.ci < sci(1)
+            userStructure.ci = userStructure.ci+1;
+       end
+    end
+end
+
+function output = pid (error, Kp, Ki, Kd, timeStep, integrates)
+    sErr = size(error);
+    output = Kp*error(sErr(2)) + Ki*integrates + Kd*(error(sErr(2))-error(sErr(2)-1))/timeStep;
 end
